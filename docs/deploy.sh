a0a5478b755b78ebd3bd62af13bb73d029f0967f
hylexus <hylexus@163.com>
2020-01-31 15:40:42 +0800

    deploy docs

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd pages/.vuepress/dist
+cd .vuepress/dist
 #
 ##git init
 git add -A
--- docs/pages/jt-808/guide/FAQ/index.md
+++
@@ -1,8 +0,0 @@
-# FAQ
-
-::: tip
-本小节内容如下：
-:::
-
-- [报文解析相关](./package-parsing.md)
-- [调试相关](./debug.md)
