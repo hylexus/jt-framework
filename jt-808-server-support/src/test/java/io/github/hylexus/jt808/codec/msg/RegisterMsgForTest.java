6187ca95b0b23c34715eea1387a2f826572152fd
hylexus <hylexus@163.com>
2021-02-14 17:49:37 +0800

    fix(FieldDecoder): JavaBeanMetadata 忽略 @Transient 标记的Field
    Closes #32

---
+++ jt-808-server-support/src/test/java/io/github/hylexus/jt808/codec/msg/RegisterMsgForTest.java
@@ -0,0 +1,57 @@
+package io.github.hylexus.jt808.codec.msg;
+
+import io.github.hylexus.jt.annotation.Transient;
+import io.github.hylexus.jt.annotation.msg.req.Jt808ReqMsgBody;
+import io.github.hylexus.jt.annotation.msg.req.basic.BasicField;
+import io.github.hylexus.jt808.msg.RequestMsgBody;
+import io.github.hylexus.jt808.msg.RequestMsgHeader;
+import io.github.hylexus.jt808.support.entity.scan.RequestMsgHeaderAware;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import static io.github.hylexus.jt.data.MsgDataType.*;
+
+/**
+ * Created At 2020-06-12 17:24
+ *
+ * @author hylexus
+ */
+@Data
+@Accessors(chain = true)
+@Jt808ReqMsgBody(msgType = 0x0100)
+public class RegisterMsgForTest implements RequestMsgBody, RequestMsgHeaderAware {
+
+    private RequestMsgHeader requestMsgHeader;
+
+    @BasicField(startIndex = 0, dataType = WORD)
+    private int provinceId;
+
+    @BasicField(startIndex = 2, dataType = WORD)
+    private int cityId;
+
+    @BasicField(startIndex = 4, dataType = BYTES, length = 5)
+    private byte[] manufacturerId;
+
+    @BasicField(startIndex = 9, dataType = BYTES, length = 20)
+    private byte[] terminalType;
+
+    @BasicField(startIndex = 29, dataType = BYTES, length = 7)
+    private byte[] terminalId;
+
+    @BasicField(startIndex = 36, dataType = BYTE)
+    private byte color;
+
+    @BasicField(startIndex = 37, dataType = STRING, byteCountMethod = "getCarIdentifierLength")
+    private String carIdentifier;
+
+    @Transient
+    private Object transientFieldTest;
+
+    public void setRequestMsgHeader(RequestMsgHeader requestMsgHeader) {
+        this.requestMsgHeader = requestMsgHeader;
+    }
+
+    public int getCarIdentifierLength() {
+        return requestMsgHeader.getMsgBodyLength() - 37;
+    }
+}
